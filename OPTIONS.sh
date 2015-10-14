770aaeb0e3e45d862c05f3b5ddaac16edf5baecc
Anders Kaseorg <andersk@mit.edu>
2015-10-14 15:49:15 -0400

    Ignore exit status 23 and 24 from rsync
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>
    Signed-off-by: Lizhou Sha <slz@mit.edu>

--- OPTIONS.sh
+++ OPTIONS.sh
@@ -1,1 +1,13 @@
 MIRRORDIR=/ifs/mirror
+
+rsync () {
+    # Ignore these exit statuses from rsync:
+    #   23  Partial transfer due to error
+    #   24  Partial transfer due to vanished source files
+    command rsync "$@" || (
+        case "$?" in
+            23 | 24) echo "mirrors.mit.edu: Ignoring rsync exit status $?" >&2;;
+            *) exit $?
+        esac
+    )
+}
